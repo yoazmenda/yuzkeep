7427d3c69c3565d67735f4c8327fabbd9052a614
yoazmenda <yoazmenda@gmail.com>
2021-05-06 12:25:52 +0300

    add secured login page + in memory user db

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -13,7 +13,6 @@ import java.util.Collection;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
 
     @Autowired
--- src/main/java/com/example/demo/controller/WebRouterController.java
+++ src/main/java/com/example/demo/controller/WebRouterController.java
@@ -1,16 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class TodoWebController {
+public class WebRouterController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
     }
 
 }
